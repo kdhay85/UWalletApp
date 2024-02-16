d8fac6195e4b566ad3a9973957d70beb3298e0e8
Kaila171 <krd78@miami.edu>
2024-02-15 21:05:01 -0500

    KAILA CAN COMMIT

--- app/src/main/java/edu/miami/karysse/mytwobuttons/HomeActivity.java
+++ app/src/main/java/edu/miami/karysse/mytwobuttons/HomeActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
-
+//KAILA
 public class HomeActivity extends AppCompatActivity {
 
     @Override
